04972d1327341a557be4711402da9b1aa009d3a8
Mohammed alotaibi <muhammedalotaibi13@gmail.com>
2018-01-25 09:44:07 +0300

    first commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by محمد عايض العتيبي on 5/8/1439 AH.
+//  Copyright © 1439 code schoole. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
